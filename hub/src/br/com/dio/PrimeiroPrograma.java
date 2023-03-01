401a493790a90a7519e967b05f0ec5940029e0ab
emival95 <102115864+emival95@users.noreply.github.com>
2023-03-01 17:06:37 -0300

    Add files via upload

---
+++ hub/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato ();
+		Livros livro = new Livros ();
+		System.out.println(gato);
+		System.out.println(livro);
+		/*int a=2;
+		int b=3;
+		System.out.println("hello world ! "  + (a+b));*/
+
+	}
+
+}
+
+	class Livros {
+		private String nome;
+		private String npag;
+	}
---
+++ hub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Valzim
+ *
+ */
+module programa {
+}
